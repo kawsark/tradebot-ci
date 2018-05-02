a3458560d2bdf664a57b1efb85c11bec94a7bf17
kawsark <azureuser@controlvm.rzswpzsd1lduvdjicbkbbnvevd.bx.internal.cloudapp.net>
2018-05-02 19:46:51 +0000

    Updated to use VM Scalesets

--- ansible/deploy_tradebotwebui.sh
+++ ansible/deploy_tradebotwebui.sh
@@ -2,7 +2,8 @@
 #Use this script to trigger ansible deployment of tradebot webui application
 #Assumes azure cli is installed and authenticated
 export ansible_hosts_file=azure-ansible-hosts
-az vm list-ip-addresses -g tradebotresourcegroup | grep \"ipAddress\"\: > azure_ip_addr.txt
+az vmss list-instance-public-ips -g tradebotresourcegroup -n tradebotwebuivmss | grep \"ipAddress\"\: > azure_ip_addr.txt
+#az vm list-ip-addresses -g tradebotresourcegroup | grep \"ipAddress\"\: > azure_ip_addr.txt
 echo Deploying application to servers: $(cat azure_ip_addr.txt)
 cat > $ansible_hosts_file <<EOL
 [local]
--- variables.tf
+++ variables.tf
@@ -1,3 +1,11 @@
+variable "azure_vm_sku" {
+	 default = "Standard_DS1_v2"
+}
+
+variable "azure_vm_qty" {
+	 default = 2
+}
+
 variable "location" {
 	 default = "eastus"
 }
