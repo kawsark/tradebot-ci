a108763f7fd6498a150d93f9f06cafadc815774c
Ubuntu <azureuser@controlvm.rzswpzsd1lduvdjicbkbbnvevd.bx.internal.cloudapp.net>
2018-05-08 04:17:25 +0000

    Updated code to use tradebot-terraform-module

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-state-tpxl7m"
-    key    = "terraform/tradebot-webui-stage.tfstate"
+    key    = "terraform/tradebot-prod.tfstate"
     region = "us-east-1"
   }
 }
