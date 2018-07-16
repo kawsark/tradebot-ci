9e5375e6867aa6b64a90c9ff0bb18923b26f7141
kawsark <kawsar@hashicorp.com>
2018-07-16 03:34:04 -0400

    Updated code to use tradebot-terraform-module

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-state-tpxl7m"
-    key    = "terraform/tradebot-prod.tfstate"
+    key    = "terraform/tradebot-dev.tfstate"
     region = "us-east-1"
   }
 }
--- main.tf
+++ main.tf
@@ -1,11 +1,26 @@
-#Provision the Tradebot application infrastructure in Azure and AWS using the tradebot module
-module "tradebot" {
-  source = "github.com/kawsark/tradebot-terraform-module"
+#Provision the Tradebot UI and DNS infrastructure in Azure
+module "tradebotui" {
+  source = "github.com/kawsark/tradebot-terraform-module//tradebot-ui-azure"
 
-  #Setting Dev specific variables
+  #Setting Production specific variables (optional - module defaults to production)
   environment = "dev"
+  location    = "eastus"
+  vault_secret_path = "secret/tradebot/dev"
   subnet_address_prefix = "10.0.4.0/24"
   domain_name_label = "tradebotuidev"
+
+  #Capacity related variables (optional)
+  azure_vm_qty  = 1
+  azure_vm_sku  = "Standard_DS1_v2"
+}
+
+#Provision the Tradebot server infrastructure in AWS
+module "tradebotserver" {
+  source = "github.com/kawsark/tradebot-terraform-module//tradebot-server-aws"
+
+  #Setting Production specific variables (optional - module defaults to production)
+  environment            = "dev"
+  aws_region             = "us-east-1"
   vault_secret_path = "secret/tradebot/dev"
   tradebot-in-queue = "tradebot-in-dev-queue-YP0MHaB"
   public_subnet_1_block = "192.168.64.0/21"
@@ -13,11 +28,12 @@ module "tradebot" {
   private_subnet_1_block = "192.168.80.0/21"
   private_subnet_2_block = "192.168.88.0/21"
 
-  #Capacity related variables
-  azure_vm_qty = 1
-  azure_vm_sku = "Standard_DS1_v2"
+  #Capacity related variables (optional)
   instance_size = "t2.micro"
-  asg_size_map = { min = 1, desired = 1, max = 1 }
 
+  asg_size_map = {
+    min = 1
+    desired = 1
+    max = 1
+  }
 }
-  
